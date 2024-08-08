07458272199ca0b8844fa6fe0e0f667a6330c921
Jeo Abarre <136715818+J10-A8ARR3@users.noreply.github.com>
2024-08-08 13:37:40 +0800

    updated

--- vue-learn/src/components/JobListings.vue
+++ vue-learn/src/components/JobListings.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { RouterLink } from 'vue-router';
 import JobListing from './JobListing.vue';
 import jobData from '@/jobs2.json';
 import { ref, defineProps } from 'vue';
@@ -10,7 +11,6 @@ defineProps({
         default: false 
     }
 });
-
 
 const jobs = ref(jobData);
 
@@ -32,10 +32,9 @@ const jobs = ref(jobData);
     </section>
 
     <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-      <a
-        href="/jobs"
+      <RouterLink
+        to="/jobs"
         class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
+        >View All Jobs</RouterLink>
     </section>
 </template>
